3f8ac9091ba65fb0d4001e2e95d1220b6157bcee
Irad Cohen <irad.cohen@sap.com>
2015-06-06 19:31:39 +0300

    areas show the map \!

--- src/main/webapp/javascripts/views/areas.js
+++ src/main/webapp/javascripts/views/areas.js
@@ -1,9 +1,10 @@
 define(
     ["backbone",
         "jquery",
+		"ol",
         "text!templates/areas.html",
         "bootstrap",
-    ], function (Backbone, $, Areas) {
+    ], function (Backbone, $, ol, Areas) {
         var AreasView = Backbone.View.extend({
             el: ".body-container",
             events: {
@@ -11,13 +12,55 @@ define(
             },
             initialize: function () {
                 this.render();
+				this.initMap();
+				this.initCategories();
             },
             render: function () {
                 var template = _.template(Areas);
                 this.$el.html(template());
-				this.getCategories();
             },
-			getCategories: function () {
+			initMap: function () {
+                var raster = new ol.layer.Tile({
+                    source: new ol.source.MapQuest({layer: 'osm'})
+                });
+
+                var map = new ol.Map({
+                    layers: [raster],
+                    target: 'map',
+                    view: new ol.View({
+                        center: ol.proj.transform([31, 37], 'EPSG:4326', 'EPSG:3857'),
+                        zoom: 4,
+                    })
+                });
+
+                var featureOverlay = new ol.FeatureOverlay({
+                    style: new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: 'rgba(255, 255, 255, 0.2)'
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: '#ffcc33',
+                            width: 2
+                        }),
+                        image: new ol.style.Circle({
+                            radius: 7,
+                            fill: new ol.style.Fill({
+                                color: '#ffcc33'
+                            })
+                        })
+                    })
+                });
+                featureOverlay.setMap(map);
+
+                draw = new ol.interaction.Draw({
+                    features: featureOverlay.getFeatures(),
+                    type: "Circle"
+                });
+                map.addInteraction(draw);
+
+                MyGlobal.circle_locations = featureOverlay;
+			},
+			initCategories: function () {
                 $.ajax({
                     url:'get_all_categories'
                     }).done(function(data){
@@ -47,6 +90,13 @@ define(
 						
 						});
 			},
+            onSubmit: function() {
+                var locationsArr = MyGlobal.circle_locations.getFeatures().getArray();
+                for (i = 0; i < MyGlobal.circle_locations.getFeatures().getLength(); i++) {
+                    console.log(ol.proj.transform(locationsArr[i].getGeometry().getCenter(), 'EPSG:3857', 'EPSG:4326'));
+                    console.log(locationsArr[i].getGeometry().getRadius());
+                }
+            },
 				/*
             onSubmit: function () {
 				var req_json = this.constructRequest();
