046162d368429c55eba644113c22d290cb3d73e3
nimrodoron <nimrodoron@gmail.com>
2015-06-09 19:00:33 +0300

    Search by location editor

---
+++ src/main/java/protocol_model/SearchByLocationEditor.java
@@ -0,0 +1,26 @@
+package protocol_model;
+
+import com.google.gson.Gson;
+import com.sun.org.apache.xml.internal.security.utils.Base64;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+
+import java.beans.PropertyEditorSupport;
+
+/**
+ * Created by nimrod on 6/6/15.
+ */
+public class SearchByLocationEditor extends PropertyEditorSupport {
+
+        @Override
+        public void setAsText(String text) throws IllegalArgumentException {
+            //String text = Base64.decode(text);
+            //Object obj = JSONValue.parse(text);
+            Gson gson = new Gson();
+            SearchByLocation sc = gson.fromJson(text, SearchByLocation.class);
+            setValue(sc);
+        }
+    }
+
+
