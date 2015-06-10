a1fd82d688447588318c18cb4899f6e2c9927db8
shirilad <shirilad@gmail.com>
2015-06-10 15:54:32 +0300

    add support of marking circle on map for choosing places

--- src/main/webapp/javascripts/views/places.js
+++ src/main/webapp/javascripts/views/places.js
@@ -1,9 +1,10 @@
 define(
     ["backbone",
         "jquery",
+        "ol",
         "text!templates/places.html",
         "bootstrap",
-    ], function (Backbone, $, PlacesTemplate) {
+    ], function (Backbone, $, ol, PlacesTemplate) {
         var PlacesView = Backbone.View.extend({
             el: ".body-container",
             events: {
@@ -16,8 +17,50 @@ define(
             render: function () {
                 var template = _.template(PlacesTemplate);
                 this.$el.html(template());
+                this.initMap();
 				this.catView.render();
 				this.initProperties();
+            },
+            initMap: function () {
+                var raster = new ol.layer.Tile({
+                    source: new ol.source.MapQuest({layer: 'osm'})
+                });
+
+                var map = new ol.Map({
+                    layers: [raster],
+                    target: 'map',
+                    view: new ol.View({
+                        center: ol.proj.transform([31, 37], 'EPSG:4326', 'EPSG:3857'),
+                        zoom: 4,
+                    })
+                });
+
+                var featureOverlay = new ol.FeatureOverlay({
+                    style: new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: 'rgba(255, 255, 255, 0.2)'
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: '#ffcc33',
+                            width: 2
+                        }),
+                        image: new ol.style.Circle({
+                            radius: 7,
+                            fill: new ol.style.Fill({
+                                color: '#ffcc33'
+                            })
+                        })
+                    })
+                });
+                featureOverlay.setMap(map);
+
+                draw = new ol.interaction.Draw({
+                    features: featureOverlay.getFeatures(),
+                    type: "Circle"
+                });
+                map.addInteraction(draw);
+
+                MyGlobal.circle_locations = featureOverlay;
             },
 			initProperties: function () {
                 $.ajax({
@@ -55,12 +98,15 @@ define(
 
             },
 			constructRequest: function () {
+                var location_circle = MyGlobal.circle_locations.getFeatures().getArray()[0];
+                var location_coordinates = ol.proj.transform(location_circle.getGeometry().getCenter(), 'EPSG:3857', 'EPSG:4326');
+
                 var cat_yago_id = $('#categories').val();
 				return {
 					"loc": {
-						"lat": $('#lat').val(),
-						"lon": $('#lon').val(),
-						"radius": $('#radius').val(),
+                        "lat": location_coordinates[0],
+                        "lon": location_coordinates[1],
+						"radius": location_circle.getGeometry().getRadius(),
 					},
 					"categories": [
                         MyGlobal.collections.categories.get(cat_yago_id).attributes
