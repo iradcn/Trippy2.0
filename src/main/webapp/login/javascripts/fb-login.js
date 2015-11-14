b301e6789feab8104ec079559de89fdd5582a51f
amirbar <amirb4r@gmail.com>
2015-11-14 14:57:40 +0200

    fix login issues

---
+++ src/main/webapp/login/javascripts/fb-login.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Amir on 14/11/2015.
+ */
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
