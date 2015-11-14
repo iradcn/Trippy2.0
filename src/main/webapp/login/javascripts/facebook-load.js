aece4144c4353b8d426f06722cef74c2db26d649
amirbar <amirb4r@gmail.com>
2015-11-14 14:05:19 +0200

    changed FB load to index

--- src/main/webapp/login/javascripts/facebook-load.js
+++ src/main/webapp/login/javascripts/facebook-load.js
@@ -3,25 +3,6 @@
  */
     // Load the SDK asynchronously
 
-(function(){
-
-    var FB_API_VERSION = 'v2.2';
-    var APP_ID = '1662478060658059';
-
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId      : APP_ID,
-            cookie     : true,  // enable cookies to allow the server to access
-            xfbml      : true,  // parse social plugins on this page
-            version    : FB_API_VERSION // use version 2.2
-        });
-    };
-
-
-
-}());
-
-
 (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
