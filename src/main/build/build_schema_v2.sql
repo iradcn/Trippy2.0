fe4960e9a9d96dd747f2bc50486f2a404d878375
amirbar <amirb4r@gmail.com>
2015-12-05 16:31:08 +0200

    schema update

--- src/main/build/build_schema_v2.sql
+++ src/main/build/build_schema_v2.sql
@@ -10,6 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema trippy2
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `trippy2` ;
 
 -- -----------------------------------------------------
 -- Schema trippy2
@@ -20,8 +21,10 @@ USE `trippy2` ;
 -- -----------------------------------------------------
 -- Table `trippy2`.`categories`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`categories` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`categories` (
-  `Id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Id` INT(10) UNSIGNED NOT NULL,
   `Name` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`Id`),
   UNIQUE INDEX `Id_UNIQUE` (`Id` ASC))
@@ -32,6 +35,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`places`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`places` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`places` (
   `n_id` INT(11) NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(200) NOT NULL,
@@ -48,11 +53,12 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`places_categories`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`places_categories` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`places_categories` (
   `PlaceId` VARCHAR(200) NOT NULL,
   `CategoryId` INT(10) NOT NULL,
-  INDEX `IX_CATEGORY_ID` (`CategoryId` ASC),
-  INDEX `FK_PLACE_ID_idx` (`PlaceId` ASC))
+  INDEX `IX_CATEGORY_ID` (`CategoryId` ASC))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -60,6 +66,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`places_props`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`places_props` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`places_props` (
   `PlaceId` VARCHAR(200) NOT NULL,
   `PropId` INT(10) UNSIGNED NOT NULL,
@@ -74,6 +82,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`properties`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`properties` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`properties` (
   `Id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `Name` VARCHAR(45) NULL DEFAULT NULL,
@@ -86,6 +96,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`user_roles`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`user_roles` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`user_roles` (
   `user_id` VARCHAR(40) NOT NULL,
   `role` VARCHAR(45) NULL DEFAULT NULL,
@@ -97,6 +109,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`users`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`users` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`users` (
   `user_id` VARCHAR(40) NOT NULL,
   `password` VARCHAR(45) NULL DEFAULT NULL,
@@ -112,6 +126,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`users_check_in`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`users_check_in` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`users_check_in` (
   `user_id` VARCHAR(40) NOT NULL,
   `place_id` VARCHAR(200) NOT NULL)
@@ -122,6 +138,8 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`users_search_props_history`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`users_search_props_history` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`users_search_props_history` (
   `user_id` VARCHAR(40) NOT NULL,
   `prop_id` INT(10) NOT NULL)
@@ -132,13 +150,16 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `trippy2`.`uservotes`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `trippy2`.`uservotes` ;
+
 CREATE TABLE IF NOT EXISTS `trippy2`.`uservotes` (
   `userId` VARCHAR(40) NOT NULL,
   `placeId` VARCHAR(40) NOT NULL,
   `propId` INT(11) NOT NULL,
   `vote` INT(11) NOT NULL,
   `fTimestamp` VARCHAR(45) NOT NULL,
-  `is_opened` BINARY(1) NOT NULL DEFAULT '0')
+  `is_opened` BINARY(1) NOT NULL DEFAULT '0',
+  `nPlaceId` INT(11) NOT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
 
@@ -155,68 +176,9 @@ CREATE TABLE IF NOT EXISTS `trippy2`.`places_props_view` (`placeId` INT, `propId
 CREATE TABLE IF NOT EXISTS `trippy2`.`user_votes_agg_view` (`placeId` INT, `propId` INT, `votesRank` INT, `max(fTimestamp)` INT);
 
 -- -----------------------------------------------------
--- procedure CreateForeignKeys
--- -----------------------------------------------------
-
-DELIMITER $$
-USE `trippy2`$$
-CREATE DEFINER=`trippy2`@`localhost` PROCEDURE `CreateForeignKeys`()
-BEGIN
-
-    ALTER TABLE placesprops
-	ADD CONSTRAINT FK_PLACE_ID
-	FOREIGN KEY (PlaceId)
-	REFERENCES places(Id);
-
-    ALTER TABLE placesprops
-	ADD CONSTRAINT FK_PROP_ID
-	FOREIGN KEY (PropId)
-	REFERENCES properties(Id);
-
-    ALTER TABLE placescategories
-	ADD CONSTRAINT FK_CATEGORY_ID_
-	FOREIGN KEY (CategoryId)
-	REFERENCES categories(YagoId);
-
-    ALTER TABLE placescategories
-	ADD CONSTRAINT FK_PLACE_ID_
-	FOREIGN KEY (PlaceId)
-	REFERENCES places(Id);
-
-END$$
-
-DELIMITER ;
-
--- -----------------------------------------------------
--- procedure RemoveForeignKeys
--- -----------------------------------------------------
-
-DELIMITER $$
-USE `trippy2`$$
-CREATE DEFINER=`trippy2`@`localhost` PROCEDURE `RemoveForeignKeys`()
-BEGIN
-
-    ALTER TABLE placesprops
-	  DROP FOREIGN KEY FK_PLACE_ID;
-
-
-    ALTER TABLE placesprops
-	  DROP FOREIGN KEY FK_PROP_ID;
-
-
-    ALTER TABLE placescategories
-	  DROP FOREIGN KEY FK_CATEGORY_ID_;
-
-    ALTER TABLE placescategories
-	  DROP FOREIGN KEY FK_PLACE_ID_;
-
-END$$
-
-DELIMITER ;
-
--- -----------------------------------------------------
 -- View `trippy2`.`places_props_view`
 -- -----------------------------------------------------
+DROP VIEW IF EXISTS `trippy2`.`places_props_view` ;
 DROP TABLE IF EXISTS `trippy2`.`places_props_view`;
 USE `trippy2`;
 CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`trippy2`@`localhost` SQL SECURITY DEFINER VIEW `trippy2`.`places_props_view` AS select `trippy2`.`uservotes`.`placeId` AS `placeId`,`trippy2`.`uservotes`.`propId` AS `propId` from `trippy2`.`uservotes` group by `trippy2`.`uservotes`.`placeId`,`trippy2`.`uservotes`.`propId` having (sum(`trippy2`.`uservotes`.`vote`) > 0);
@@ -224,6 +186,7 @@ CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`trippy2`@`localhost` SQL SECURIT
 -- -----------------------------------------------------
 -- View `trippy2`.`user_votes_agg_view`
 -- -----------------------------------------------------
+DROP VIEW IF EXISTS `trippy2`.`user_votes_agg_view` ;
 DROP TABLE IF EXISTS `trippy2`.`user_votes_agg_view`;
 USE `trippy2`;
 CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`trippy2`@`localhost` SQL SECURITY DEFINER VIEW `trippy2`.`user_votes_agg_view` AS select `trippy2`.`uservotes`.`placeId` AS `placeId`,`trippy2`.`uservotes`.`propId` AS `propId`,sum(`trippy2`.`uservotes`.`vote`) AS `votesRank`,max(`trippy2`.`uservotes`.`fTimestamp`) AS `max(fTimestamp)` from `trippy2`.`uservotes` group by `trippy2`.`uservotes`.`placeId`,`trippy2`.`uservotes`.`propId`;
--- src/main/java/DAO/VoteDAO.java
+++ src/main/java/DAO/VoteDAO.java
@@ -20,7 +20,7 @@ import model.Vote;
 public class VoteDAO {
     private static String InsertVotePropToPlace = "INSERT INTO uservotes (userId, placeId, propId, vote, fTimestamp, placenid) Values(?,?,?,?,?,?)";
     private static String selectOpenQuestion = "SELECT * FROM uservotes where is_opened = 0 + userId =? ";
-    private static String voteQuestion = "";
+    private static String voteQuestion = "UPDATE uservotes set `";
     public static void insertNewQuestion(int propId, String placeId, int voteValue, String username, long nId) throws SQLException {
         Connection conn = JDBCConnection.getConnection();
         PreparedStatement addVotePropPlace = conn.prepareStatement(InsertVotePropToPlace);
