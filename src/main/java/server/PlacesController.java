3ae93af2b1972955b253f4ec3c75eb1e48c25114
amirbar <amirb4r@gmail.com>
2015-12-05 11:52:00 +0200

    updated with todos

--- src/main/java/server/PlacesController.java
+++ src/main/java/server/PlacesController.java
@@ -1,19 +1,26 @@
 package server;
 
 import model.Place;
+import model.Vote;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import protocol_model.*;
+import services.QuestionManagerService;
 
 import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 public class PlacesController {
+	
+	@Autowired
+	QuestionManagerService newQuestionManager;
+	
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.registerCustomEditor(SearchByLocation.class, new SearchByLocationEditor());
@@ -26,8 +33,18 @@ public class PlacesController {
 	}
 
 	@RequestMapping(value="app/get_places_by_loc", method=RequestMethod.POST)
-	public List<Place> SearchPlacesByLocation(@RequestBody SearchByLocation searchQueryJson) throws SQLException {
-		return Place.getPlacesByLocation(searchQueryJson);
+	public QuestionAndResults SearchPlacesByLocation(@RequestBody SearchByLocation searchQueryJson) throws SQLException {
+		
+		
+		Vote newQuestion = newQuestionManager.getQuestion();
+		/*
+		 * 
+		 * if we have question return the question else get data and return data
+		 * Place.getPlacesByLocation(searchQueryJson);
+		 * 
+		 */
+		//TODO: If user will recieve data now, increment the counter 
+		return null;
 	}
 
 	@RequestMapping(value="app/get_places_aggregation", method=RequestMethod.POST)
